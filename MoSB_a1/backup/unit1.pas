a20daa49e1b61f1b4b212f0c90c94fb84d8850cd
Ol Kovalenko <404udp@gmail.com>
2021-09-17 17:46:21 +0300

    небольшие штрихи

--- MoSB_a1/backup/unit1.pas
+++ MoSB_a1/backup/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  FileUtil, Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  FileUtil, ShellApi, Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
 
@@ -34,21 +34,24 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var arh1, arh2, file_s:TStringList;
   dir1, dir2:string;
-  i,i2:integer;
-  path_temp, path_temp2, path_temp3:string;
+  i,i2, i3 , i4:integer;
+  path_temp, path_temp2, path_temp3, path_temp4, path_out:string;
   temi2:string;
   file_ar, file_exct:string;
   str_name_file:string;
-  arr_name_file:TStringArray;
+ arr_name_file:TStringArray;
 begin
 path_temp:='l:\temp_ship';
 str_name_file:='acc.data,adam.data,boiler.data,gps.data,gpsriver.data,t.data';
 arr_name_file:=str_name_file.Split(',');//получаем массив нужных файлов
 path_temp2:=path_temp+'\shipBox\sent';
 path_temp3:=path_temp+'\all';
+path_temp4:=path_temp+'\all\shipBox\temp';
+path_out:=path_temp+'\out\';
 temi2:='';
 CreateDir(path_temp);
 CreateDir(path_temp3);
+CreateDir(path_out);
 //    dir1
 if SelectDirectoryDialog1.Execute then
     //Listbox1.Items.Assign(SelectDirectoryDialog1.Files);
@@ -76,12 +79,34 @@ do
 begin
 //ShowMessage(arh2[i2]);
 //распаковsваем каждый и объединяем
-
 file_exct:='Y:\!monitoring\unpacker1\7z.exe x '+arh2[i2]+' -o"'+path_temp3+'" -y';
 ExecuteProcess(file_exct, '');
 //а теперь смотрим что у нас там....
 // arr_name_file список того что нужно брать
-
+//ShowMessage(path_temp3);
+file_s:=FindAllFiles(path_temp4,'*',false);
+//получили список распакованніх файлов
+for i3:=0 to file_s.Count-1
+do
+begin
+//ShowMessage(file_s[i3]);
+for i4:=0 to Length(arr_name_file)-1
+do
+begin //если находим искомый файл - объединаем его в общий
+if (file_s[i3].IndexOf(arr_name_file[i4])>=0) then
+   begin //copy 1.data+2.data /b 1.data
+file_exct:='cmd.exe /K ';
+//ExecuteProcess(file_exct, 'copy '+path_out+arr_name_file[i4]+'+'+file_s[i3]+' /b '+path_out+arr_name_file[i4]);
+//if ShellExecute(0,nil, PChar('cmd'),PChar('/k copy'+path_out+arr_name_file[i4]+'+'+file_s[i3]+' /b '+path_out+arr_name_file[i4]),nil,1) =0 then;
+if ShellExecute(0,nil, PChar('cmd'),PChar('/c copy '+path_out+arr_name_file[i4]+'+'+file_s[i3]+' /b '+path_out+arr_name_file[i4]),nil,0)=0 then;
+Sleep(25);
+//ExecuteProcess(file_exct, 'dir');
+//ShowMessage(PChar(file_s[i3]));
+   end
+//ShowMessage(IntToStr(file_s[i3].IndexOf(arr_name_file[i4]))+file_s[i3]);
+end;
+end;
+DeleteDirectory(path_temp3, True);
 
 temi2:=temi2+arh2[i2];
 end;
